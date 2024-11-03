0771a63f27fcf500836bfe3a085df1c9d5ba75fb
lukabrankovic <89201035@student.upr.si>
2024-11-03 22:02:43 +0100

    added dist to repository

---
+++ backend/dist/main.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+const path_1 = require("path");
+const config_1 = require("@nestjs/config");
+const platform_express_1 = require("@nestjs/platform-express");
+const express = require("express");
+const server = express();
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_express_1.ExpressAdapter(server));
+    const configService = app.get(config_1.ConfigService);
+    console.log('Connected to database:', configService.get('MONGODB_URI'));
+    app.enableCors();
+    app.useStaticAssets((0, path_1.join)(__dirname, '..', 'public'));
+    app.use('*', (req, res) => {
+        res.sendFile((0, path_1.join)(__dirname, '..', 'public', 'index.html'));
+    });
+    app.use((req, res, next) => {
+        console.log(`Request received: ${req.method} ${req.url}`);
+        next();
+    });
+    await app.init();
+    await app.listen(3000, '0.0.0.0');
+}
+bootstrap();
+exports.default = server;
+//# sourceMappingURL=main.js.map
