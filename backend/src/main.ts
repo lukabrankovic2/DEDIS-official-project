2ea60f210fbf7bc78523f3aab80300270d82a6b4
lukabrankovic <89201035@student.upr.si>
2024-11-03 22:33:50 +0100

    debugging package json

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,29 +6,26 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const configService = app.get(ConfigService);
-
-  console.log('Connected to database:', configService.get('MONGODB_URI'));
-
-  // Enable CORS
   app.enableCors();
 
-  // Serve static files from the public directory
+  // Serve static files
   app.useStaticAssets(join(__dirname, '..', 'public'));
 
-  // Middleware for logging requests
+  // Middleware for logging
   app.use((req, res, next) => {
     console.log(`Request received: ${req.method} ${req.url}`);
     next();
   });
 
-  // Catch-all route for client-side routing
+  // Catch-all route
   app.use('*', (req, res) => {
     res.sendFile(join(__dirname, '..', 'public', 'index.html'));
   });
 
-  // Initialize and listen for serverless compatibility
   await app.init();
+  return app;
 }
 
-bootstrap();
+const appPromise = bootstrap();
+
+export default appPromise;
