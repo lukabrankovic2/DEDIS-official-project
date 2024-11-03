db4a663bf0d4537aa7eca99ac4ea24cd42c7777d
lukabrankovic <89201035@student.upr.si>
2024-11-03 19:19:26 +0100

    added database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes environment variables available throughout the app
+    }),
+    DatabaseModule,
+  ],
+  controllers: [AppController], // Register the default AppController
+  providers: [AppService], // Register the default AppService
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
 
+   console.log('Connected to database:', configService.get('MONGODB_URI'));
   app.enableCors
   // Serve static files from the public directory
   app.useStaticAssets(join(__dirname, '..', 'public'));
