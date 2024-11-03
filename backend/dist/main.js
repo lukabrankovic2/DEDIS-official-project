d839b1523f016efd8e29632e26138d00e9721f98
lukabrankovic <89201035@student.upr.si>
2024-11-03 22:26:19 +0100

    removing express

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -4,11 +4,8 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const path_1 = require("path");
 const config_1 = require("@nestjs/config");
-const platform_express_1 = require("@nestjs/platform-express");
-const express = require("express");
-const server = express();
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_express_1.ExpressAdapter(server));
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
     const configService = app.get(config_1.ConfigService);
     console.log('Connected to database:', configService.get('MONGODB_URI'));
     app.enableCors();
@@ -23,5 +20,4 @@ async function bootstrap() {
     await app.init();
 }
 bootstrap();
-exports.default = server;
 //# sourceMappingURL=main.js.map
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,16 +3,9 @@ import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-
-const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(
-    AppModule,
-    new ExpressAdapter(server)
-  );
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
 
   console.log('Connected to database:', configService.get('MONGODB_URI'));
@@ -34,11 +27,8 @@ async function bootstrap() {
     res.sendFile(join(__dirname, '..', 'public', 'index.html'));
   });
 
-  // Initialize the app (serverless compatibility)
+  // Initialize and listen for serverless compatibility
   await app.init();
 }
 
 bootstrap();
-
-// Export the server for Vercel to use
-export default server;
