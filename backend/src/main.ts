b763f2cbd41768a177ebaca90324f91c4cc7095d
lukabrankovic <89201035@student.upr.si>
2024-11-05 19:50:16 +0100

    added env to src folder

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,6 @@ import { AppModule } from './app.module';
 import { join } from 'path';
 import * as express from 'express';
 
-// esgidrugkusdgsdhjlidghlifdhlgi
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(express.static(join(__dirname, '..', 'public')));
