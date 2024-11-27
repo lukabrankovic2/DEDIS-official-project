258a906a855ad4a2f2402bb501eaee9dea7137ad
89201287 <89201287@student.upr.si>
2024-11-27 11:30:45 +0100

    working on fronend and the look of the page

--- frontend/src/routes/Signin.svelte
+++ frontend/src/routes/Signin.svelte
@@ -1,6 +1,37 @@
 <script>
-    //logic
-  </script>
-  
-  <h1>Sign In Here</h1>
-  
+  let email = '';
+  let password = '';
+
+  function handleSubmit() {
+    console.log('Username:', email);
+    console.log('Password:', password);
+  }
+</script>
+
+<div class="signin-container">
+  <form class="signin-form" on:submit|preventDefault={handleSubmit}>
+    <div class="form-group">
+      <label for="email">Email</label>
+      <input
+        type="text"
+        id="username"
+        bind:value={email}
+        placeholder="Enter your email"
+        required
+      />
+    </div>
+
+    <div class="form-group">
+      <label for="password">Password</label>
+      <input
+        type="password"
+        id="password"
+        bind:value={password}
+        placeholder="Enter your password"
+        required
+      />
+    </div>
+
+    <button type="submit">Sign In</button>
+  </form>
+</div>
