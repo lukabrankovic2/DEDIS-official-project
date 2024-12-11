235d29346c3a23417fbd7f71396346d9eef35d0a
89201287 <89201287@student.upr.si>
2024-12-11 14:20:58 +0100

    popravljala frontend tj home page, ako nesto ne radi ovaj commit je kriv

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -1,8 +1,19 @@
-<script>
-    
-  </script>
-  
-  <div class="home-content">
-    <img src="/logo2w.png" alt="Background Logo" class="background-logo" />
-    <h1>Welcome to the Homepage!</h1>
-  </div>
+<div class="home-content">
+  <img src="/logo2w.png" alt="Background Logo" class="background-logo" />
+  <h1>Welcome to the OAK official website!</h1>
+</div>
+
+<div class="text-box">
+  <p>scroll down</p>
+  <p>important info here</p>
+  <p>important info here</p>
+  <p>important info here</p>
+  <p>important info here</p>
+  <p>important info here</p>
+  <p>important info here</p>
+  <p>important info here</p>
+  <p>important info here</p>
+  <p>important info here</p>
+  <p>important info here</p>
+
+</div>
