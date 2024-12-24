09e35d493120f2548ea4ade9faaf89df10a1247e
lukabrankovic <89201035@student.upr.si>
2024-12-24 22:18:53 +0100

    testing connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
 
 @Module({
   imports: [
