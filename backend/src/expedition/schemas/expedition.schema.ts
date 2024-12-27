bea77990aa0fea2c3f68167ed13d40bdd2e7b533
lukabrankovic <89201035@student.upr.si>
2024-12-27 15:00:42 +0100

    added expeditions

--- backend/src/expedition/schemas/expedition.schema.ts
+++ backend/src/expedition/schemas/expedition.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
 
-@Schema()
+@Schema({ timestamps: true })
 export class Expedition extends Document {
   @Prop({ required: true })
   title: string;
--- frontend/src/routes/Expeditions.svelte
+++ frontend/src/routes/Expeditions.svelte
@@ -1,6 +1,26 @@
 <script>
+  import { onMount } from 'svelte';
   import { user } from '../stores/userStore.js';
   import { currentPage } from '../stores/pageStore.js';
+
+  let expeditions = [];
+
+  const fetchExpeditions = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/expeditions');
+      if (response.ok) {
+        expeditions = await response.json();
+      } else {
+        console.error('Failed to fetch expeditions');
+      }
+    } catch (error) {
+      console.error('Error fetching expeditions:', error);
+    }
+  };
+
+  onMount(() => {
+    fetchExpeditions();
+  });
 
   const navigateToCreateExped = () => {
     console.log('Navigating to CreateExped'); // Debug statement
@@ -8,8 +28,19 @@
   };
 </script>
 
-<h1> Testing!!! </h1>
+<h1>Expeditions</h1>
 
 {#if $user}
   <button on:click={navigateToCreateExped}>Create Expedition</button>
 {/if}
+
+{#each expeditions as expedition}
+  <div class="expedition">
+    <h2>{expedition.title}</h2>
+    <p><strong>Posted by:</strong> {expedition.user.username}</p>
+    <p><strong>Date:</strong> {new Date(expedition.createdAt).toLocaleDateString()}</p>
+    <p><strong>Members:</strong> {expedition.members}</p>
+    <p><strong>Route:</strong> {expedition.route}</p>
+    <p><strong>Description:</strong> {expedition.description}</p>
+  </div>
+{/each}
