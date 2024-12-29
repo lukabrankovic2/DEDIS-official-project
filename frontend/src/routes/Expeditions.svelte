59f44eff58f8f728cd898bebf1012c3f3acccc04
lukabrankovic <89201035@student.upr.si>
2024-12-29 22:29:48 +0100

    changed back

--- frontend/src/routes/Expeditions.svelte
+++ frontend/src/routes/Expeditions.svelte
@@ -8,7 +8,7 @@
   // Dynamically determine the backend URL
   const BACKEND_URL = window.location.hostname === 'localhost'
     ? 'http://localhost:3000'
-    : 'https://dedis-official-project.vercel.app';
+    : 'https://dedis-official-project.vercel.app/api';
 
   const fetchExpeditions = async () => {
     try {
